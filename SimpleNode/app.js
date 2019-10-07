715ad31952269c99c4ddd825496404dd5f1fc18c
Radu Albastroiu <radu.albastroiu@caphyon.com>
2019-10-08 01:16:48 +0300

    Add simple node.js project

---
+++ SimpleNode/app.js
@@ -0,0 +1,7 @@
+// Create a webserver
+
+// Handle http route GET / and POST / i.e. Home
+
+// Handle http route GET /:username i.e. /chalkers
+
+// Function that handles the reading of files and merg in value
