4e32ee509a9a0d081cf308ec7db675aef8ff7947
Radu Albastroiu <albastroiuradug@gmail.com>
2019-10-08 01:30:26 +0300

    Create server

--- SimpleNode/app.js
+++ SimpleNode/app.js
@@ -1,5 +1,15 @@
 // Create a webserver
 
+var http = require("http");
+http
+  .createServer(function(request, response) {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.write("This is before the end\n");
+    response.end("Hello World\n");
+  })
+  .listen(3000);
+console.log("Server running on port 3000");
+
 // Handle http route GET / and POST / i.e. Home
 
 // Handle http route GET /:username i.e. /chalkers
