fafe96728d6539a070a318eb0970569ba6261b5b
Radu Albastroiu <albastroiuradug@gmail.com>
2019-10-11 01:23:46 +0300

    Add node express

---
+++ NodeExpress/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+const colors = ["red", "orange", "yellow", "green", "blue", "purple"];
+
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/cards", (req, res) => {
+  res.render("card", { prompt: "Who is burried", hint: "A hint" });
+});
+
+app.listen(3000, () => {
+  console.log("The application is working on port 3000");
+});
