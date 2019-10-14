a52a6761f7eafe99cbfa14061360d3174f97ac94
Radu Albastroiu <albastroiuradug@gmail.com>
2019-10-14 08:50:12 +0300

    Using request query

--- NodeExpress/routes/cards.js
+++ NodeExpress/routes/cards.js
@@ -1,12 +1,30 @@
 const express = require("express");
+const data = require("../data/flashcardData.json").data;
+const cards = data.cards;
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("card", {
-    prompt: "Who is burried",
-    hint: "A hint"
-  });
+  const questionId = Math.floor(Math.random() * 5);
+  res.redirect(`/cards/${questionId}?side=question`);
+});
+
+router.get("/:id", (req, res) => {
+  const { side } = req.query;
+  const { id } = req.params;
+  const text = cards[id][side];
+  const { hint } = cards[id];
+  const reverseSide = side === "question" ? "answer" : "question";
+  const templateData = {
+    text,
+    id,
+    sideToShow: reverseSide,
+    sideToShowDisplay: reverseSide
+  };
+  if (side === "question") {
+    templateData.hint = hint;
+  }
+  res.render("card", templateData);
 });
 
 module.exports = router;
