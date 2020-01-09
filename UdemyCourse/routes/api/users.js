fe1a3a02b13f2f6bb934f6b29113c3fda8ff9be1
Radu Albastroiu <albastroiuradug@gmail.com>
2020-01-09 15:12:32 +0200

    Send jwt token

--- UdemyCourse/routes/api/users.js
+++ UdemyCourse/routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../../models/User');
@@ -60,8 +62,23 @@ router.post(
       await user.save();
 
       // return jwt
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
 
-      return res.send('User registered');
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) {
+            throw err;
+          }
+          res.json({ token: token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       return res.status(500).send('Server error');
