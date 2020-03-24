ed76592a2f4f79ab7f8744521283096a7b2944d3
Radu Albastroiu <albastroiuradug@gmail.com>
2020-03-24 23:19:30 +0200

    Serialize and deserialize user

--- UdemyCourseReactNode/server/services/passport.js
+++ UdemyCourseReactNode/server/services/passport.js
@@ -5,6 +5,16 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -15,10 +25,15 @@ passport.use(
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then(existingUser => {
         if (existingUser) {
+          // already in database
+          done(null, existingUser);
         } else {
+          // not in database yet
           new User({
             googleId: profile.id
-          }).save();
+          })
+            .save()
+            .then(user => done(null, user));
         }
       });
     }
