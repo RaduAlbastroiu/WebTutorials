ebf8b82d724a05d7b8a76723061d254a71b66d25
Radu Albastroiu <albastroiuradug@gmail.com>
2020-03-24 22:21:54 +0200

    Add user model

--- UdemyCourseReactNode/server/services/passport.js
+++ UdemyCourseReactNode/server/services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 passport.use(
   new GoogleStrategy(
@@ -10,10 +13,13 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log({
-        accesToken: accessToken,
-        refreshToken: refreshToken,
-        profile: profile
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+        } else {
+          new User({
+            googleId: profile.id
+          }).save();
+        }
       });
     }
   )
