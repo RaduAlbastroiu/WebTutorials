5641f6bea3eaad82f08b7dc26e8b858f0d86b48a
Radu Albastroiu <albastroiuradug@gmail.com>
2020-03-26 23:51:33 +0200

    Restyle button

--- UdemyCourseReactNode/server/client/src/components/Payments.js
+++ UdemyCourseReactNode/server/client/src/components/Payments.js
@@ -11,7 +11,9 @@ class Payments extends Component {
         token={token => console.log(token)}
         // it is a public key, should be in env
         stripeKey={'pk_test_HkKV4H0AnGWpjeJQaXvH1vhA00bCJJ8K9x'}
-      />
+      >
+        <button className="btn">Add Credits</button>
+      </StripeCheckout>
     );
   }
 }
