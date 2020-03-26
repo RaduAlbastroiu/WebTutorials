73e2688b0a12ee39f93f590c8f57fdc10166365b
Radu Albastroiu <albastroiuradug@gmail.com>
2020-03-26 23:48:45 +0200

    Add stripe checkout

---
+++ UdemyCourseReactNode/server/client/src/components/Payments.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+class Payments extends Component {
+  render() {
+    return (
+      <StripeCheckout
+        name="Emaily"
+        description="$$$"
+        amount={499}
+        token={token => console.log(token)}
+        // it is a public key, should be in env
+        stripeKey={'pk_test_HkKV4H0AnGWpjeJQaXvH1vhA00bCJJ8K9x'}
+      />
+    );
+  }
+}
+
+export default Payments;
