4145cc2fb8f057c9caa4a40f40182b14fbc50eb9
Radu Albastroiu <albastroiuradug@gmail.com>
2020-03-28 23:32:14 +0200

    Render survey form

--- UdemyCourseReactNode/server/client/src/components/surveys/SurveyForm.js
+++ UdemyCourseReactNode/server/client/src/components/surveys/SurveyForm.js
@@ -1,9 +1,44 @@
+// SurveyForm shos a form for a user to add input
+
 import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+import SurveyField from './SurveyField';
+import _ from 'lodash';
+
+const FIELDS = [
+  { label: 'Survey Title', name: 'title' },
+  { label: 'Subject Line', name: 'subject' },
+  { label: 'Email Body', name: 'body' },
+  { label: 'Recipient List', name: 'email' }
+];
 
 class SurveyForm extends Component {
+  renderFields() {
+    return _.map(FIELDS, ({ label, name }) => {
+      return (
+        <Field
+          key={name}
+          component={SurveyField}
+          type="text"
+          label={label}
+          name={name}
+        />
+      );
+    });
+  }
+
   render() {
-    return <div>SurveyForm!</div>;
+    return (
+      <div>
+        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+          {this.renderFields()}
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
   }
 }
 
-export default SurveyForm;
+export default reduxForm({
+  form: 'surveyForm'
+})(SurveyForm);
