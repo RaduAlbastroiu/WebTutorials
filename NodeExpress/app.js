54d279db7757425e7ef63ccd8015e5980318b957
Radu Albastroiu <albastroiuradug@gmail.com>
2021-10-10 18:41:20 +0300

    Fix rule

--- NodeExpress/app.js
+++ NodeExpress/app.js
@@ -1,32 +1,32 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use("/static", express.static("public"));
-app.set("view engine", "pug");
+app.use('/static', express.static('public'));
+app.set('view engine', 'pug');
 
-const mainRoutes = require("./routes");
-const cardsRoutes = require("./routes/cards");
+const mainRoutes = require('./routes');
+const cardsRoutes = require('./routes/cards');
 
 app.use(mainRoutes);
-app.use("/cards", cardsRoutes);
+app.use('/cards', cardsRoutes);
 
 app.use((req, res, next) => {
-  console.log("Hello");
+  console.log('Hello');
   next();
 });
 
 app.use((req, res, next) => {
-  console.log("World");
+  console.log('World');
   next();
 });
 
 app.use((req, res, next) => {
-  const err = new Error("Not found");
+  const err = new Error('Not found');
   err.status = 404;
   next(err);
 });
@@ -34,9 +34,9 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   res.locals.error = err;
   res.status(err.status);
-  res.render("error");
+  res.render('error');
 });
 
 app.listen(3000, () => {
-  console.log("The application is working on port 3000");
+  console.log('The application is working on port 3000');
 });
